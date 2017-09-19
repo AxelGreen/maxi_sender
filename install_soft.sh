e74cd8a73ac5039f86348b24ead2bde5df10360e
Axel Green <axelgreenkp@gmail.com>
2017-09-19 15:31:51 +0300

    remove centos support

--- install_soft.sh
+++ install_soft.sh
@@ -30,55 +30,9 @@ function debian_like {
     fi
 
     apt-get update
-    apt-get install -y fail2ban
+    apt-get install -y fail2ban htop
     apt-get install -y postgresql-9.6 erlang erlang-nox rabbitmq-server exim4
     apt-get install -y --allow-unauthenticated php7.0 php7.0-opcache php7.0-mbstring php7.0-bcmath php7.0-zip php7.0-geoip php7.0-curl php7.0-json php7.0-pgsql php7.0-cli
-}
-
-function rhel_like {
-    yum update -y
-    which wget > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        yum install -y wget
-    fi
-
-    local centos6=$(cat /etc/centos-release | grep -i "CentOS release 6")
-    if [ -n "${centos6}" ]; then
-        # postgresql
-        rpm -Uvh https://yum.postgresql.org/9.6/redhat/rhel-6-x86_64/pgdg-redhat96-9.6-3.noarch.rpm
-
-        # rabbitmq
-        wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.9/rabbitmq-server-3.6.9-1.el6.noarch.rpm -O rabbitmq.rpm
-
-        # php 7
-        rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
-        wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-        rpm -ivh epel-release-6-8.noarch.rpm
-        rm -f epel-release-6-8.noarch.rpm
-    else
-        # postgresql
-        rpm -Uvh https://yum.postgresql.org/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
-
-        # rabbitmq
-        wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.9/rabbitmq-server-3.6.9-1.el7.noarch.rpm -O rabbitmq.rpm
-
-        # php7
-        rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-        rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-    fi
-
-    # erlang
-    wget https://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
-    rpm -Uvh erlang-solutions-1.0-1.noarch.rpm
-    rm -f erlang-solutions-1.0-1.noarch.rpm
-
-    rpm --import https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
-
-    yum update -y
-    yum install -y postgresql96-server postgresql96 erlang exim fail2ban rabbitmq-server --skip-broken
-    yum install -y php70w php70w-gd php70w-opcache php70w-mbstring php70w-bcmath php70w-common php70w-pgsql php70w-cli
-    yum install -y rabbitmq.rpm
-    rm -f rabbitmq.rpm
 }
 
 function sudo_check {
@@ -89,16 +43,4 @@ function sudo_check {
 }
 
 sudo_check
-
-managers=("apt-get" "yum")
-for m in ${managers[@]}; do
-    which $m > /dev/null 2>&1
-    if [ $? -eq 0 ] && [ "$m" == "apt-get" ]; then
-        debian_like
-        break
-    fi
-    if [ $? -eq 0 ] && [ "$m" == "yum" ]; then
-        rhel_like
-        break
-    fi
-done
+debian_like
--- uninstall_soft.sh
+++ uninstall_soft.sh
@@ -16,22 +16,6 @@ function debian_like {
     done
 }
 
-function rhel_like {
-    local patterns=("erlang" "postgres" "php7" "rabbitmq" "exim", "fail2ban")
-    for pattern in ${patterns[@]}; do
-        packages=$(rpm -qa | grep ${pattern})
-        if [ -z "${packages}" ]; then
-            continue
-        fi
-        for file in $(rpm -q --configfiles ${packages}); do
-            if [ -f ${file} ]; then
-                rm -f ${file}
-            fi
-        done
-        rpm -e ${packages} 2>/dev/null
-    done
-}
-
 function sudo_check {
     if [ "$EUID" -ne 0 ]; then
         echo "Please script as root"
@@ -40,16 +24,4 @@ function sudo_check {
 }
 
 sudo_check
-
-managers=("apt-get" "yum")
-for m in ${managers[@]}; do
-    which $m > /dev/null 2>&1
-    if [ $? -eq 0 ] && [ "$m" == "apt-get" ]; then
-        debian_like
-        break
-    fi
-    if [ $? -eq 0 ] && [ "$m" == "yum" ]; then
-        rhel_like
-        break
-    fi
-done
+debian_like
