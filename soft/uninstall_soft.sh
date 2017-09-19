3e7a29bd09070e49e0326d20e89f31f74b956033
Axel Green <axelgreenkp@gmail.com>
2017-09-19 17:31:35 +0300

    delete all parts for OS diff

--- soft/uninstall_soft.sh
+++ soft/uninstall_soft.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-function debian_like {
-    which add-apt-repository > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        apt-get install -y software-properties-common
-    fi
+if [ "$EUID" -ne 0 ]; then
+    echo "Please script as root"
+    exit 1
+fi
 
-    add-apt-repository --remove -y ppa:ondrej/php
-    add-apt-repository --remove -y precise-pgdg
-    add-apt-repository --remove -y testing
+which add-apt-repository > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    apt-get install -y software-properties-common
+fi
 
-    local patterns=("erlang" "postgres" "php7" "rabbitmq" "exim" "fail2ban")
-    for pattern in ${patterns[@]}; do
-        apt-get --purge remove -y $(dpkg -l | grep ${pattern} | awk -F ' ' '{print $2}')
-    done
+add-apt-repository --remove -y ppa:ondrej/php
+add-apt-repository --remove -y precise-pgdg
+add-apt-repository --remove -y testing
 
-    rm -rf /usr/local/bin/composer
-}
+patterns=("erlang" "postgres" "php7" "rabbitmq" "exim" "fail2ban")
+for pattern in ${patterns[@]}; do
+    apt-get --purge remove -y $(dpkg -l | grep ${pattern} | awk -F ' ' '{print $2}')
+done
 
-function sudo_check {
-    if [ "$EUID" -ne 0 ]; then
-        echo "Please script as root"
-        exit 1
-    fi
-}
-
-sudo_check
-debian_like
+rm -rf /usr/local/bin/composer
