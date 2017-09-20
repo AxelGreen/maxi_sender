24eb53c7653a301b88afc58cc86febb713bd62ad
Axel Green <axelgreenkp@gmail.com>
2017-09-20 12:03:25 +0300

    asd

--- config/exim.sh
+++ config/exim.sh
@@ -5,9 +5,8 @@ sed -i "s/^.*dc_eximconfig_configtype.*=.*$/dc_eximconfig_configtype='internet'/
 sed -i "s/^.*dc_localdelivery.*=.*$/dc_localdelivery='\/var\/mail'/" /etc/exim4/update-exim4.conf.conf
 # interfaces (IPs) which Exim will listen to. Empty value means that listen all interfaces
 sed -i "s/^.*dc_local_interfaces.*=.*$/dc_local_interfaces=''/" /etc/exim4/update-exim4.conf.conf
-# TODO: create /etc/exim4/domains.virtual file which contains all domains connected to this VPS (one per line)
 # list of domains which Exim will consider as itself (get emails for this domains, not relay them)
-sed -i "s/^.*dc_other_hostnames.*=.*$/dc_other_hostnames='\/etc\/exim4\/domains\.virtual'/" /etc/exim4/update-exim4.conf.conf
+sed -i "s/^.*dc_other_hostnames.*=.*$/dc_other_hostnames='*'/" /etc/exim4/update-exim4.conf.conf
 # exim will use split config files
 sed -i "s/^.*dc_use_split_config.*=.*$/dc_use_split_config='true'/" /etc/exim4/update-exim4.conf.conf
 # set retry config. one minute after error occurred, 6 minutes after error occurred, 31 minute after error occurred
