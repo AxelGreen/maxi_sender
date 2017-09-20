cec09079a210ed420bc7f89f7e9ed54b125b7292
Axel Green <axelgreenkp@gmail.com>
2017-09-20 14:50:27 +0300

    asd

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# run deinstallation for all soft - to be sure that all is clear and new one
 ./soft/uninstall_soft.sh
+# run installation
 ./soft/install_soft.sh
+# run all configuration change files
+./config/exim.sh
