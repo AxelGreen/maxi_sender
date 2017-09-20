5ee113f44968ecfda9d3bd9ae6257fefbb79d12b
Axel Green <axelgreenkp@gmail.com>
2017-09-20 19:10:58 +0300

    install update

--- config/postgres.sh
+++ config/postgres.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-# TODO: complete this
+# listed connection from all IPs
+sed -i "s/^.*listen_addresses.*$/listen_addresses = '*'/" /etc/postgresql/9.6/main/postgresql.conf
+# trust connections from central server. Don't ask password
+echo "host    postgres        postgres        88.99.195.32/0  trust" >> /etc/postgresql/9.6/main/pg_hba.conf
+# TODO: initialize tables for statistics and maybe something else
 
+# restart postgres to apply changes
 /etc/init.d/postgresql restart
--- config/rabbit.sh
+++ config/rabbit.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 # TODO: complete
+
+# restart rabbit to apply changes
+/etc/init.d/rabbitmq-server restart
