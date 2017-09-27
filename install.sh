998238816bfedff2385fd8bb3c77872888e82332
Axel Green <axelgreenkp@gmail.com>
2017-09-27 11:11:28 +0300

    installer update

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+echo Installation start: $(date)
+
 # check if root running this script
 if [ "$EUID" -ne 0 ]; then
     echo "Run script as root"
     exit 1
 fi
 
+echo Soft installation
 # run deinstallation for all soft - to be sure that all is clear and new one
 ./soft/uninstall_soft.sh
 # run installation
 ./soft/install_soft.sh
 
+echo Configuration
 # run all configuration change files
 ./config/exim.sh
 ./config/postgres.sh
@@ -18,13 +22,21 @@ fi
 ./config/rabbit.sh
 ./config/php.sh
 
+echo Scripts
 # remove all scripts
 ./soft/uninstall_scripts.sh
 # install php scripts
 ./soft/install_scripts.sh
 
+echo Configure
 # run script to configure some soft and scripts: change configuration file for scripts (set passwords, etc), get dkims and domains.virtual for Exim, create db for Postgres
 php7.0 ./scripts/php/configure.php
 
+echo Cron
 # update cron
 ./config/cron.sh
+
+echo Installation end: $(date)
+echo Cleanup files
+# cleanup installation files
+./soft/cleanup.sh
