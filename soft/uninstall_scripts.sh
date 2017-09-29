5cf7d2288aad320ceee541e34b1889ee35b6b1b1
Axel Green <axelgreenkp@gmail.com>
2017-09-29 13:46:17 +0300

    installer update

--- soft/uninstall_scripts.sh
+++ soft/uninstall_scripts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # stop mutator
 pkill --full "^tail.*/var/log/exim4/mainlog$"
+#stop send.php
+pkill --full "^php7\.0.*/etc/sender4you/send\.php$"
 # clear folder with scripts and logs
 rm -rf /etc/sender4you /var/log/sender4you
