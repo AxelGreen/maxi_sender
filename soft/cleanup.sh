fd641125e4790abdb5b0eaa3cc1f5d15aadde63f
Axel Green <axelgreenkp@gmail.com>
2017-09-29 13:47:16 +0300

    installer update

--- soft/cleanup.sh
+++ soft/cleanup.sh
@@ -5,6 +5,5 @@ cp /root/maxi_sender/install.log /var/log/sender4you/install.log
 rm -rf /root/maxi_sender/
 rm -rf /root/install_run.sh
 rm -rf /root/install_run.log
-rm -rf /etc/sender4you/bash/crontab_template
 # purge git
 apt-get purge -y git
