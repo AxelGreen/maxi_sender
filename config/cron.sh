e35cafac629536b5c6ab050e56b62f2f126b6a78
Axel Green <axelgreenkp@gmail.com>
2017-09-29 12:00:04 +0300

    installer update

--- config/cron.sh
+++ config/cron.sh
@@ -2,6 +2,6 @@
 # drop current crontab
 crontab -r
 # initialize new crontab from template file
-crontab /root/maxi_sender/scripts/bash/crontab_template
+crontab /etc/sender4you/bash/crontab_template
 # restart cron to apply changes
 /etc/init.d/cron restart
