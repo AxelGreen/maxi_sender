41d5a85d6a7df9dfe5366dcdf912baaec6ed9002
Axel Green <axelgreenkp@gmail.com>
2017-09-29 10:47:38 +0300

    installer update

---
+++ scripts/php/src/Sender4you/Configure/Postgres.php
@@ -0,0 +1,40 @@
+<?php
+
+    namespace Sender4you\Configure;
+
+    use Common\Connection\PgConnection;
+
+    class Postgres
+    {
+
+        public function run()
+        {
+
+            $connection = PgConnection::getInstance();
+
+            $this->createTables($connection);
+
+        }
+
+        private function createTables(PgConnection $connection)
+        {
+
+            // create logs table
+            $query
+                = '
+                CREATE TABLE IF NOT EXISTS exim_logs (
+                    date TIMESTAMP WITH TIME ZONE NOT NULL,
+                    exim_id VARCHAR(16) NOT NULL PRIMARY KEY,
+                    action SMALLINT NOT NULL,
+                    message_id VARCHAR NULL,
+                    host VARCHAR NULL,
+                    error VARCHAR NULL
+                )
+            ';
+
+            // execute query
+            $connection->query($query);
+
+        }
+
+    }
