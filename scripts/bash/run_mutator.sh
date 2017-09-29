8d1b81c246e145db9c6a623626b1a0078c1efbd5
Axel Green <axelgreenkp@gmail.com>
2017-09-29 11:37:15 +0300

    installer update

--- scripts/bash/run_mutator.sh
+++ scripts/bash/run_mutator.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-MUTATOR_COUNT=${pgrep --full --count "^tail.*/var/log/exim4/mainlog$"}
-echo "$MUTATOR_COUNT"
+MUTATOR_COUNT=$(pgrep --full --count "^tail.*/var/log/exim4/mainlog$")
+
+# check if mutator already running
+if [ $MUTATOR_COUNT -gt 0 ]; then
+	exit 1;
+fi
+
+# run mutator
+nohup /etc/sender4you/bash/mutator.sh 2>&1 1>/dev/null &
+disown
