8b2fbf1390324f223009bc63bb7db868675a8e93
Axel Green <axelgreenkp@gmail.com>
2017-10-06 16:58:32 +0300

    installer update

--- scripts/bash/run_distributor.sh
+++ scripts/bash/run_distributor.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-SENDERS_COUNT=$(pgrep --full --count "^php7\.0.*/etc/sender4you/distributor\.php$")
+# connect to memcached
+source /etc/sender4you/bash/membash.sh
+MCSERVER="localhost"
+MCPORT=11211
+
+# get value of active_in_pool
+ACTIVE=$(mc_get active_in_pool)
+REG="0"
+echo "${ACTIVE}"
+if [[ ${ACTIVE} =~ $REG ]] # not active in pool
+then
+	exit 1;
+fi
+
+# get number of distributors running
+DISTRIBUTORS_COUNT=$(pgrep --full --count "^php7\.0.*/etc/sender4you/distributor\.php$")
 
 # check if distributor already running
-if [ $SENDERS_COUNT -ge 1 ]; then
+if [ $DISTRIBUTORS_COUNT -ge 1 ]; then
 	exit 1;
 fi
 
