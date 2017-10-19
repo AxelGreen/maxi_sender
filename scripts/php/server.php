197e65457b545a797c54ae9fdc36308246c6f6d0
Axel Green <axelgreenkp@gmail.com>
2017-10-19 15:32:39 +0300

    installer update

--- scripts/php/server.php
+++ scripts/php/server.php
@@ -8,9 +8,16 @@
     }
 
     // mail.ru postmaster check
-    if (preg_match('%/mailru-verification([^\.]+)\.html%', $current_url, $rez)) {
+    if (preg_match('%/mailru-verification([^\.]{16})\.html%', $current_url, $rez)) {
 
         echo 'mailru-verification: '.$rez[1];
+
+    }
+
+    // yandex post office check
+    if (preg_match('%/([^\.]{64})\.html%', $current_url, $rez)) {
+
+        echo 'postoffice-'.$rez[1];
 
     }
 
