abe411ba86ab4a3368fecae62f3cb758fb98ee0b
Axel Green <axelgreenkp@gmail.com>
2017-10-27 15:52:53 +0300

    installer update

--- scripts/php/pause.php
+++ scripts/php/pause.php
@@ -3,6 +3,7 @@
     use Common\Connection\MemcachedConnect;
     use Sender4you\Distributor\BigBuffer;
     use Sender4you\Log\Error;
+    use Sender4you\Log\Info;
     use Sender4you\Pause\Helper;
 
     require_once __DIR__.'/vendor/autoload.php';
@@ -15,26 +16,26 @@
         // validate exim_id and retrieve sending_host for this message from DB
         $sending_host = $helper->retrieveHost($argv[1]);
         if (empty($sending_host)) {
-            return false;
+            throw new Exception(_('Empty sending host'));
         }
 
         // validate bounce and get email_domain
         $email_domain = $helper->parseBounce($argv[2]);
         if (empty($email_domain)) {
-            return false;
+            throw new Exception(_('Empty email domain'));
         }
 
         // retrieve bounce_limits from Memcache or central server (if there is no local copy)
         $bigs_buffer = BigBuffer::getInstance();
         $bounce_limits = $bigs_buffer->getBounceLimits();
         if (empty($bounce_limits)) { // no data about limits, so can't process
-            return false;
+            throw new Exception(_('Bounce limits'));
         }
 
         // detect Big
         $big_id = $helper->detectBigId($email_domain, $bounce_limits);
         if (empty($big_id)) { // Big not detected
-            return;
+            throw new Exception(_('Big not determined'));
         }
 
         // retrieve previous bounces from Memcache
@@ -48,6 +49,8 @@
         // save previous_bounces to memcache
         $helper->savePreviousBounces($sending_host, $big_id, $previous_bounces);
 
+        Info::push(_('Bounce for').' '.$sending_host.' big_id: '.$big_id.' '._('collected. Total count:').' '.count($previous_bounces), 'pause');
+
         // check bounces limit
         if (count($previous_bounces) < $bounce_limits[$big_id]['bounce_limit']) { // not enough bounces for pause
             return true;
@@ -59,6 +62,8 @@
         $restore_at = microtime(true) + ($bounce_limits[$big_id]['pause'] * 3600);
         // save 1 to memcache with expiration time (when we can restore sending). So if this key is not set in memcache - we can continue to send this
         $memcached->set($cache_key, $restore_at, $bounce_limits[$big_id]['pause'] * 3600, false);
+
+        Info::push(_('Pause for').' '.$sending_host.' big_id: '.$big_id.' '._('is set until').' '.$restore_at.' ('.date('Y-m-d H:i:s', $restore_at).')', 'pause');
 
     } catch (Exception $ex) {
 
