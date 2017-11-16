979e847f3242699329632c35134fe718e7807349
Axel Green <axelgreenkp@gmail.com>
2017-11-16 14:49:04 +0200

    installer update

--- soft/install_soft.sh
+++ soft/install_soft.sh
@@ -47,8 +47,7 @@ apt-get install -y exim4
 apt-get install -y erlang erlang-nox rabbitmq-server
 # php7.0 and it's extensions
 apt-get install -y --allow-unauthenticated php7.0 php7.0-opcache php7.0-mbstring php7.0-bcmath php7.0-zip php7.0-geoip php7.0-curl php7.0-json php7.0-pgsql php7.0-cli php7.0-memcached
-
-apt-get purge -y apache2
+# try to fix all apt installation errors
 apt-get install -f -y
 
 # composer
