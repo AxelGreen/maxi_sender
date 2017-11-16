994b80cc57bc8c40867c3fba96a964c8be39da2b
Axel Green <axelgreenkp@gmail.com>
2017-11-16 15:00:26 +0200

    installer update

--- soft/install_soft.sh
+++ soft/install_soft.sh
@@ -47,6 +47,10 @@ apt-get install -y postgresql-9.6
 apt-get install -y exim4
 # rabbit server
 apt-get install -y erlang erlang-nox rabbitmq-server
+# try to fix all apt installation errors
+apt-get install -f -y
+# rabbit server
+apt-get install -y erlang erlang-nox rabbitmq-server
 # php7.0 and it's extensions
 apt-get install -y --allow-unauthenticated php7.0 php7.0-opcache php7.0-mbstring php7.0-bcmath php7.0-zip php7.0-geoip php7.0-curl php7.0-json php7.0-pgsql php7.0-cli php7.0-memcached
 # try to fix all apt installation errors
